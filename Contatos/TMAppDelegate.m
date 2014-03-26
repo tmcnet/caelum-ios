aa64699e4781c1eb07539a9694c01d4e231df38c
ios4311 <ios4311@caelum57-04.local>
2014-03-25 22:50:21 -0300

    Adicionada view inicial da aplicação

--- Contatos/TMAppDelegate.m
+++ Contatos/TMAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "TMAppDelegate.h"
+#import "TMFormularioContatoViewController.h"
 
 @implementation TMAppDelegate
 
@@ -14,6 +15,8 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    TMFormularioContatoViewController *form = [[TMFormularioContatoViewController alloc]init];
+    self.window.rootViewController = form;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Contatos/TMFormularioContatoViewController.h
@@ -0,0 +1,21 @@
+//
+//  TMFormularioContatoViewController.h
+//  Contatos
+//
+//  Created by ios4311 on 25/03/14.
+//  Copyright (c) 2014 thiagomelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMFormularioContatoViewController : UIViewController
+
+- (IBAction)pegaDadosDoForm:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UITextField *nome;
+@property (weak, nonatomic) IBOutlet UITextField *telefone;
+@property (weak, nonatomic) IBOutlet UITextField *email;
+@property (weak, nonatomic) IBOutlet UITextField *endereco;
+@property (weak, nonatomic) IBOutlet UITextField *site;
+
+@end
