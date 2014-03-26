c9e8e3cfe187dea0cd517b76fb6dbd48e02e4c32
ios4311 <ios4311@caelum57-04.local>
2014-03-25 21:38:49 -0300

    Initial Commit

---
+++ Contatos/TMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMAppDelegate.h
+//  Contatos
+//
+//  Created by ios4311 on 25/03/14.
+//  Copyright (c) 2014 thiagomelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Contatos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Contatos
+//
+//  Created by ios4311 on 25/03/14.
+//  Copyright (c) 2014 thiagomelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
